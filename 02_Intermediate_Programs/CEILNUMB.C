c017b0b98e9b9391fa092b87bf30131bbfa934f1
Nithya-Alfonisa <nithsnisha@gmail.com>
2025-01-16 13:04:21 +0530

    Add files via upload

---
+++ 02_Intermediate_Programs/CEILNUMB.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+void main()
+{
+float a;
+float b;
+clrscr();
+printf("ENter value of a:\n");
+scanf("%f",&a);
+printf("ENter value of b:\n");
+scanf("%f",&b);
+printf("value of a:%f\n",ceil(a));
+printf("value of b:%f\n",ceil(b));
+getch();
+}
